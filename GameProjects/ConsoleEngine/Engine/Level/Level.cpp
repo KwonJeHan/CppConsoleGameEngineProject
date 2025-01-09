05169f6853acd8431ffe7c9ce59114805591c314
KwonJeHan <rnjswpgks@hknu.ac.kr>
2025-01-09 17:51:55 +0900

    게임 엔진 프로그래밍 4일차
    화면 지우기(Clear)
    
    List 자료구조에 삭제 함수(Erase) 추가
    
    Actor/Level/Engine 클래스에서 액터 제거하는 함수 추가
    
    문자열 흘리기 데모 -> %(모듈러 연산)

--- GameProjects/ConsoleEngine/Engine/Level/Level.cpp
+++ GameProjects/ConsoleEngine/Engine/Level/Level.cpp
@@ -21,11 +21,28 @@ void Level::AddActor(Actor* newActor)
 	actors.PushBack(newActor);
 }
 
+void Level::DestroyActor()
+{
+	// 액터 순회 후 삭제 요청된 액터를 처리
+	for (int ix = 0; ix < actors.Size(); ++ix)
+	{
+		delete actors[ix];
+		actors[ix] = nullptr;
+		actors.Erase(ix);
+	}
+}
+
 void Level::Update(float deltaTime)
 {
 	// 레벨에 포함된 액터를 순회하면서 Update 함수 호출
 	for (Actor* actor : actors)
 	{
+		// 액터가 비활성화 상태이거나, 삭제 요청된 경우 건너뛰기
+		if (!actor->isActive || actor->isExpired)
+		{
+			continue;
+		}
+
 		actor->Update(deltaTime);
 	}
 }
@@ -35,6 +52,12 @@ void Level::Draw()
 	// 레벨에 포함된 액터를 순회하면서 Draw 함수 호출
 	for (Actor* actor : actors)
 	{
+		// 액터가 비활성화 상태이거나, 삭제 요청된 경우 건너뛰기
+		if (!actor->isActive || actor->isExpired)
+		{
+			continue;
+		}
+
 		actor->Draw();
 	}
 }
