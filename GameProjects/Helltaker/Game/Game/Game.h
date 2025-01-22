06444e6d7a5b0c98e2024ee7671309ebad136b6e
KwonJeHan <rnjswpgks@hknu.ac.kr>
2025-01-23 01:29:41 +0900

    프로젝트 정리

---
+++ GameProjects/Helltaker/Game/Game/Game.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Engine/Engine.h>
+#include "Level/GameLevel.h"
+
+class Game : public Engine
+{
+public:
+	Game();
+	~Game();
+
+	void ToggleMenu();
+
+	static Game& Get() { return *instance; }
+
+	void NextLevel();
+
+	int index = 0;
+
+private:
+	bool showMenu = false;
+
+	Level* menuLevel = nullptr;
+	Level* backLevel = nullptr;
+
+private:
+	static Game* instance;
+};
